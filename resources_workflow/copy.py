1b7a7dfb37c1324dc0df5a3bcd738977159498e3
sric <lzqiong@gmail.com>
2020-06-29 10:27:13 +0800

    copy files

--- resources_workflow/copy.py
+++ resources_workflow/copy.py
@@ -19,33 +19,20 @@ def copy(confResourcesLocal, platform, source, target):
 	if not path.exists(target):
 		os.makedirs(target)
 
-	allFiles = configLocal['common']['files']
-	if allFiles:
-		allFiles.extend(configLocal[platform]['files'] or [])
-	else:
-		allFiles = configLocal[platform]['files']
+	doCopy(configLocal['common']['files'], configLocal['common']['ignore'])
+	doCopy(configLocal[platform]['files'], configLocal[platform]['ignore'])
 
-	allIgnoreFiles = configLocal['common']['ignore']
-	if allIgnoreFiles:
-		allIgnoreFiles.extend(configLocal[platform]['ignore'] or [])
-	else:
-		allIgnoreFiles = configLocal[platform]['ignore']
+def isIgnoreFile(file, ignoreFiles):
+	for ignoreFile in ignoreFiles:
+		if ignoreFile in file:
+			print('file %s is ignored.' % file)
+			return True
+	return False
 
-	if not allFiles:
-		print('no resources to copy')
-		return
-	
-	print(allFiles)
-	print(allIgnoreFiles)
-
-	def isIgnoreFile(file):
-		for ignoreFile in allIgnoreFiles:
-			if ignoreFile in file:
-				print('file %s is ignored.' % file)
-				return True
-		return False
-
-	for f in allFiles:
+def doCopy(includeFiles, ignoreFiles):
+	print(includeFiles)
+	print(ignoreFiles)
+	for f in includeFiles:
 		s = path.join(source, f)
 		t = path.join(target, f)
 		# print('copy from %s to %s' % (s, t))
@@ -54,7 +41,7 @@ def copy(confResourcesLocal, platform, source, target):
 		else:
 			fileList = [path.join(root, fn).replace('\\','/') for root, dirs, files in os.walk(s) for fn in files]
 			for ss in fileList:
-				if isIgnoreFile(ss):
+				if isIgnoreFile(ss, ignoreFiles):
 					continue
 				dirname = path.dirname(ss)
 				filename = path.basename(ss)
