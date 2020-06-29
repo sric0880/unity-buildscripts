2cc2a3ce3efc26c695b2c76a6a9f3723ef25f7e3
sric <lzqiong@gmail.com>
2020-06-29 10:50:23 +0800

    copy files

--- resources_workflow/copy.py
+++ resources_workflow/copy.py
@@ -19,19 +19,23 @@ def copy(confResourcesLocal, platform, source, target):
 	if not path.exists(target):
 		os.makedirs(target)
 
-	doCopy(configLocal['common']['files'], configLocal['common']['ignore'])
-	doCopy(configLocal[platform]['files'], configLocal[platform]['ignore'])
+	doCopy(configLocal['common']['files'], configLocal['common']['ignore'], source, target)
+	doCopy(configLocal[platform]['files'], configLocal[platform]['ignore'], source, target)
 
 def isIgnoreFile(file, ignoreFiles):
+	if ignoreFiles is None:
+		return False
 	for ignoreFile in ignoreFiles:
 		if ignoreFile in file:
 			print('file %s is ignored.' % file)
 			return True
 	return False
 
-def doCopy(includeFiles, ignoreFiles):
-	print(includeFiles)
-	print(ignoreFiles)
+def doCopy(includeFiles, ignoreFiles, source, target):
+	print("Copy Files: ", includeFiles)
+	print("Ingored Files: ", ignoreFiles)
+	if includeFiles is None:
+		return
 	for f in includeFiles:
 		s = path.join(source, f)
 		t = path.join(target, f)
